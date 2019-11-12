926507c9c4e290c7fe3420bcd49e57ac72146832
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 20:07:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,28 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
+  self.column_names.each do |column|
+      attr_accessor column.to_sym
     end
-  end
-
-  def self.table_name
-      self.to_s.downcase.pluralize
-    end
-
-    def self.column_names
-    DB[:conn].results_as_hash = true
-
-    sql = "pragma table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
-  end
-
 end
