bd1b38c7247e020a38b7041919ecf45015809b86
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 20:09:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,6 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   self.column_names.each do |column|
-      attr_accessor column.to_sym
-    end
+    attr_accessor column.to_sym
+  end
 end
